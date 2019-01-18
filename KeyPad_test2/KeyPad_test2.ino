993ba671d25d417f494957c69867d212178465e9
denisov0gleb <denisov0gleb@gmail.com>
2019-01-18 13:27:39 +0300

    Press 'A' to see the current value and 'B' to clear it [dgl]

--- KeyPad_test2/KeyPad_test2.ino
+++ KeyPad_test2/KeyPad_test2.ino
@@ -1,13 +1,19 @@
 #include <Keypad.h>
 
 /*
+ * Return:
+ *      @value
  * Press numbers to add value.
- * Press '*' to make a value negative and '#' overwise possitive.
- * Return 0 if unknown symbol ('*' or '#' standalone or symbols 'A', 'B', 'C', 'D').
+ * Press '*' to return a negative value. 
+ * Press '#' to return a possitive value.
+ * Press 'A' to see the value without its returning.
+ * Press 'B' to clear value.
+ * 
+ * Return 0 if unknown symbol ('*' or '#' standalone or symbols 'C', 'D').
  */
 
 String line = "";
-int value;
+long value;
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
@@ -19,8 +25,8 @@ char keys[ROWS][COLS] =
   {'*','0','#','D'}
 };
 
-byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {A2, A3, A4, A5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
@@ -38,17 +44,37 @@ void loop()
     if ( isDigit(key) )
     {
       line += key;
+      Serial.print("\tadded: ");
+      Serial.println(key);
     }
     else
     {
-      value = line.toInt();
+      
       if (key == '*')
       {
+        value = line.toInt();
         value *= -1;
+        Serial.print("Value is: ");
+        Serial.println(value);
+        line = "";
       }
-      Serial.print("There is a number: ");
-      Serial.println(value);
-      line = "";
+      if (key == '#')
+      {
+        value = line.toInt();
+        Serial.print("Value is: ");
+        Serial.println(value);
+        line = "";
+      }
+      if (key == 'A')
+      {
+        Serial.print("Not ready value: ");
+        Serial.println(line);
+      }
+      if (key == 'B')
+      {
+        Serial.println("\tClear");
+        line = "";
+      }
     }
   }
 }
